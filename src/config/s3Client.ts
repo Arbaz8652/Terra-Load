f265401601721b777aeb28bbc1943060e878bb19
Arbaz Pathan <parbaz313@gmail.com>
2025-10-07 16:32:37 +0530

    env renamed

--- src/config/s3Client.ts
+++ src/config/s3Client.ts
@@ -1,9 +1,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import 'dotenv/config';
 
 export const s3Client = new S3Client({
     region: process.env.REGION,
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+        accessKeyId: process.env.ACCESS_KEY_ID_1 as string,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY_1 as string,
     }
 })
