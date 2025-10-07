70f3e722cda87f5f7120b869ecd9348170b951d6
Arbaz Pathan <parbaz313@gmail.com>
2025-10-07 12:33:59 +0530

    feat: misc changes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import uploadRoutes from './controller/singned_url.js'
 import { cors } from 'hono/cors'
+import 'dotenv/config';
+
 const app = new Hono()
 
 app.use(
